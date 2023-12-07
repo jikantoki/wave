6e7c9a4cf11fda97a035de1344248a4f7081cc91
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-12-07 15:53:49 +0900

    JS側微調整

---
+++ pages/post/[postId].vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+
+</template>
+
+<script>
+import mixins from '~/mixins/mixins'
+import Setup from '~/js/setup'
+
+export default {
+  mixins: [mixins],
+  setup() {
+    const route = useRoute()
+    const postId = route.params.postId
+    //サーバーサイドで仮のタイトルを設定、mountedで言語ごとに再設定する
+    Setup.setTitle(postId)
+    Setup.setDescription(`ID:${postId}詳細ページ`)
+  },
+  data() {
+    return {
+      postData: null,
+    }
+  },
+}
+</script>
