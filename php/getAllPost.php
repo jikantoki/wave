f5a4553ed67e8761012fb6bbaa9915d2203e9c7d
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-12-12 10:17:08 +0900

    add sort

--- php/getAllPost.php
+++ php/getAllPost.php
@@ -4,7 +4,7 @@ require_once './settings.php'; //ルートディレクトリ読み込み
 require_once DIR_ROOT . '/php/myAutoLoad.php'; //自動読み込み
 require_once DIR_ROOT . '/php/functions/authAPIforUse.php'; //APIが有効かどうか自動判定
 
-$res = SQLfetchAll('select * from USER_POST_VIEW where isSecret=0 order by createdAt desc;');
+$res = SQLfetchAll('select * from USER_POST_VIEW where isSecret=0 order by postCreatedAt desc;');
 echo json_encode([
   'status' => 'ok',
   'reason' => 'thank you!',
